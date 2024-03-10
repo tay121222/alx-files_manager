9df83170fa3e016f84b8c9e941c33c321da3bda9
Enoch Taylor <tay121222@gmail.com>
2024-03-10 20:30:03 +0000

    Redis utils - 1

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,11 @@
 import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+    this.client.getAsync = promisify(this.client.get).bind(this.client);
+    this.client.setexAsync = promisify(this.client.setex).bind(this.client);
     this.client.on('error', (error) => {
       console.error('Error:', error);
     });
@@ -13,16 +16,13 @@ class RedisClient {
   }
 
   async get(key) {
-    return new Promise((resolve) => {
-      this.client.get(key, (error, value) => {
-        resolve(value);
-      });
-    });
+    return this.client.getAsync(key);
   }
 
   async set(key, value, duration) {
-    this.client.setex(key, duration, value);
+    return this.client.setexAsync(key, duration, value);
   }
 }
+
 const redisClient = new RedisClient();
 export default redisClient;
